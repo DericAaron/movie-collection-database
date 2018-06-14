6a4a76faebad722c166c86e1cca9125b35f93a3f
Deric Aaron <aaro0034@umn.edu>
2018-06-14 12:26:58 -0500

    app created on client, views working

---
+++ server/public/scripts/client.js
@@ -0,0 +1,13 @@
+const app = angular.module('app', ['ngRoute']);
+
+app.config(function($routeProvider){
+    $routeProvider.when('/', {
+        templateUrl: 'views/home.html'
+    }).when('/add_movie', {
+        templateUrl: 'views/addmovie.html'
+    }).when('/manage_genre', {
+        templateUrl: 'views/managegenre.html'
+    }).otherwise({
+        template: '<h2>404 Page Not Found</h2>'
+    });
+});
