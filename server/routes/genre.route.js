c36f2af7c960ce92ac608d67758413d98d671344
Deric Aaron <aaro0034@umn.edu>
2018-06-14 11:57:06 -0500

    POST and DELETE calls working on routes

--- server/routes/genre.route.js
+++ server/routes/genre.route.js
@@ -11,6 +11,31 @@ router.get('/', (req, res) => {
         }).catch((err) => {
             res.sendStatus(500);
         });
-});
+}); //end GET
+
+router.post('/', (req, res) => {
+
+    const genre = req.body
+    const queryText = 'INSERT INTO genre (genre_name) VALUES($1)';
+    pool.query(queryText, [genre.genre_name])
+        .then( (result) => {
+            console.log('genre posted succesfully');
+            res.sendStatus(200);
+        }).catch( (error) => {
+            res.sendStatus(500);
+        });
+}); //end POST
+
+router.delete('/:id', (req, res) => {
+     const id = req.params.id;
+     const queryText = 'DELETE FROM genre WHERE id=$1';
+     pool.query(queryText, [id])
+        .then( (result) => {
+            console.log('GENRE DELETED', id);
+            res.sendStatus(200);
+        }).catch( (err) => {
+            res.sendStatus(500);
+        });
+}); //end DELETE
 
 module.exports = router;
--- server/routes/movie.route.js
+++ server/routes/movie.route.js
@@ -13,4 +13,29 @@ router.get('/', (req, res) => {
         });
 });
 
+router.post('/', (req, res) => {
+
+    const movie = req.body
+    const queryText = 'INSERT INTO movie (title, genre_id) VALUES($1, $2)';
+    pool.query(queryText, [movie.title, movie.genre_id])
+        .then( (result) => {
+            console.log('movie posted succesfully');
+            res.sendStatus(200);
+        }).catch( (error) => {
+            res.sendStatus(500);
+        });
+}); //end POST
+
+router.delete('/:id', (req, res) => {
+     const id = req.params.id;
+     const queryText = 'DELETE FROM movie WHERE id=$1';
+     pool.query(queryText, [id])
+        .then( (result) => {
+            console.log('MOVIE DELETED', id);
+            res.sendStatus(200);
+        }).catch( (err) => {
+            res.sendStatus(500);
+        });
+}); //end DELETE
+
 module.exports = router;
